8a72b2c36d2bcdc640b657ba30efdaffa3d1a3ed
Pinar Kaya <35434040+pkaya89@users.noreply.github.com>
2024-01-15 12:20:55 +0000

    Add steermouse settings

---
+++ scripts/backup-steermouse.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source config.txt
+
+cp "/Users/$USER/Library/Application Support/SteerMouse & CursorSense/" steermouse/Device.smsetting
---
+++ scripts/restore-steermouse.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source config.txt
+
+cp steermouse/Device.smsetting "/Users/$USER/Library/Application Support/SteerMouse & CursorSense/"
